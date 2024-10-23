a51e3b476cc370a25f70664be4e42174c5d12278
Robby <69889440+robbysabo@users.noreply.github.com>
2024-10-23 14:20:17 -0600

    Add files via upload

---
+++ experience.js
@@ -0,0 +1,1 @@
+const d={labels:["HTML/CSS","JavaScript","C# (.NET)","VueJs","ReactJs","AWS"],datasets:[{backgroundColor:["#c96249","#e48050","#eec16c","#819e89","#82b2cd","#a08ebd"],data:[100,90,80,70,50,20]}]},opt={responsive:!0,maintainAspectRatio:!1},ctx=document.getElementById("myChart");new Chart(ctx,{type:"doughnut",data:d,options:opt});
